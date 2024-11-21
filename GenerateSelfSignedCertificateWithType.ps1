6e17bb061c2c073bae59bef9bd6bee905e3314ac
FaronicsProServices <proservices@faronics.com>
2024-11-22 00:00:45 +0530

    Update GenerateSelfSignedCertificateWithType.ps1

--- GenerateSelfSignedCertificateWithType.ps1
+++ GenerateSelfSignedCertificateWithType.ps1
@@ -1,2 +1,3 @@
 # This script generates a self-signed certificate for the specified DNS names, stores it in the specified certificate store, and sets its type.
+# New-SelfSignedCertificate -DnsName "fd.local","www.microsoft.com" -CertStoreLocation "cert:\LocalMachine\My" -Type "DocumentEncryptionCert"
 New-SelfSignedCertificate -DnsName "www.example1.com", "www.example2.com" -CertStoreLocation "Path to certificate store" -Type "CertificateType"
