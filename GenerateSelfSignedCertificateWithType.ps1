687d74050307e29eb2e5c34bfe849adda89c6b49
FaronicsProServices <proservices@faronics.com>
2024-11-22 00:00:28 +0530

    Create GenerateSelfSignedCertificateWithType.ps1

---
+++ GenerateSelfSignedCertificateWithType.ps1
@@ -0,0 +1,2 @@
+# This script generates a self-signed certificate for the specified DNS names, stores it in the specified certificate store, and sets its type.
+New-SelfSignedCertificate -DnsName "www.example1.com", "www.example2.com" -CertStoreLocation "Path to certificate store" -Type "CertificateType"
